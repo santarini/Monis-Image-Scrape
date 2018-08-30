b30679dfbd2cb6f487b1cebb01d12dab800a0811
Makoa <32736507+santarini@users.noreply.github.com>
2018-08-30 11:19:51 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-#!Python
+#! python3
+
+import os
 import requests
 import bs4 as bs
 import urllib.request
 import re
 
-url = 'https://www.bbcgoodfood.com/recipes/2239/ultimate-fish-cakes'
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
+url = 'https://www.foodnetwork.com/recipes/food-network-kitchen/cheesecake-recipe2-2011459'
+device = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+
+#os.makedirs('name', exist_ok=True)
+
+headers = {'User-Agent': device}
 resp = requests.get(url, headers=headers)
 soup = bs.BeautifulSoup(resp.text, 'lxml')
-images = soup.findAll('img')
+images = soup.findAll('img',{"src":True})
 
 i=1
 for image in images:
     if image['src'].startswith('//'):
         imageSource = image['src'][2:]
+        imageSource = "http://" + imageSource
         print(imageSource)
     else:
         imageSource = image['src']
-    #urllib.request.urlretrieve(imageSource, str(i) + ".jpg")
-    #i +=1
+        
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', device)]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(imageSource, str(i) + ".jpg")
+    i +=1
